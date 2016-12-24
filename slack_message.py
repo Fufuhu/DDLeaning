f009bb96f81ba7861cf6999a5744c9dc50a8a58f
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2016-12-25 00:17:35 +0900

    Slackにメッセージを送付するためのクラスを作成

---
+++ slack_message.py
@@ -0,0 +1,44 @@
+"""Slackにメッセージを送るためのクラス"""
+import unittest
+import slackweb
+
+
+class SlackMessanger:
+    """Slackにメッセージを送付するためのクラス"""
+
+    def __init__(self, urls):
+        self._urls = [] + urls
+
+        for url in self._urls:
+            self._slacks = [] + slackweb.Slack(url=url)
+
+    def send_messege(self, message):
+        """"Slackにメッセージを送信するメソッド"""
+        for slack in self._slacks:
+            slack.notify(text=message)
+
+    def _get_urls(self):
+        """メッセージ送付先のURLのリストを取得するメソッド"""
+        return self._urls
+
+    def _set_urls(self, urls):
+        """メッセージ送付先のURLのリストを指定するメソッド"""
+        self._urls = urls
+
+    urls = property(_get_urls, _set_urls)
+
+
+class TestSlackMessanger(unittest.TestCase):
+    """SlackMessangerをテストするためのクラス"""
+
+    def test_url(self):
+        """URLが正しく設定可能かをチェックする。"""
+
+        url = ["test_url"]
+        messanger = SlackMessanger("null")
+        messanger.urls = url
+        self.assertEqual(messanger.urls, url)
+
+
+if __name__ == '__main__':
+    unittest.main()
